bca0d7573ab96f88bcb7ead70265843c17b5f9a0
Kyle Harrison <silent.coyote1@gmail.com>
2012-03-30 21:39:20 -0700

    New router! check out app/config/router.php for details

--- httpdocs/engine/engine.php
+++ httpdocs/engine/engine.php
@@ -5,10 +5,12 @@
 * Include Manifest
 */
 $autoload = new stdClass();
+$route = array();
 
 $includes = array(
 		$ApplicationFolder."/config/config",
 		$ApplicationFolder."/config/autoload",
+		$ApplicationFolder."/config/router",
 		$EngineFolder."/plugins/load",
 		$EngineFolder."/plugins/uri",
 		$EngineFolder."/plugins/database"
@@ -46,6 +48,22 @@ foreach($autoload as $type=>$list) {
 }
 
 $uri = new uri();
+
+// need to setup router here
+foreach($route as $routeFrom=>$routeTo) {
+	$routeFrom = explode("/", $routeFrom);
+	$routeTo = explode("/", $routeTo);
+	if(!$uri->isempty()) {
+		if($uri->controller == $routeFrom[0]) {
+			$uri->controller = $routeTo[0];
+			if($uri->method != null && isset($routeFrom[1]) && $uri->method == $routeFrom[1]) { // routed controller exists
+				$uri->method = $routeTo[1];
+			}
+		}
+	}
+}
+
+
 if($uri->isempty()) {
 	load::controller($Config->DefaultController);
 	$controller = new $Config->DefaultController;
