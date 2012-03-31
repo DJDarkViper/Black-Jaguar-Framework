ac5b70da438a0ee6fa8df38fa8434af7ef771d01
Kyle Harrison <silent.coyote1@gmail.com>
2012-03-30 19:44:05 -0700

    post() function now can cleanse array data from forms, willg et to get and request soon

--- httpdocs/engine/assistants/input.php
+++ httpdocs/engine/assistants/input.php
@@ -10,11 +10,21 @@ function request($key = null) {
 }
 function post($key = null) {
 	if($key == null) {
-		foreach($_POST as &$k) { $k = addslashes(trim($k)); }
+		foreach($_POST as &$k) { 
+			if(is_array($k) || is_object($k)) {										// if an array, we must cleanse each node
+				$r = $k;															// temporarily assign
+				foreach($r as $key=>$value) $r[$key] = addslashes(trim($value)); 	// cleanse value of temp
+				$k = $r;															// reassign to be used
+			} else 	$k = addslashes(trim($k)); 
+		}
 		return (object) $_POST;
 	}
-	if(isset($_REQUEST[$key]))
-		return addslashes(trim($_POST[$key]));
+	if(isset($_POST[$key]))
+		if(is_array($_POST[$key]) || is_object($_POST[$key])) { // cleanse an array
+			$ret = $_POST[$key];
+			foreach($ret as &$r) $r = addslashes(trim($r));
+			return $ret;
+		} else return addslashes(trim($_POST[$key])); // other value
 	else return false;
 }
 function get($key = null) {
@@ -22,7 +32,7 @@ function get($key = null) {
 		foreach($_GET as &$k) { $k = addslashes(trim($k)); }
 		return (object) $_GET;
 	}
-	if(isset($_REQUEST[$key]))
+	if(isset($_GET[$key]))
 		return addslashes(trim($_GET[$key]));
 	else return false;
 }
