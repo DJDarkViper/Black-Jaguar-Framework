8d83534b6ca80c2c504e9fb414b1f5316da66e92
Kyle Harrison <silent.coyote1@gmail.com>
2012-03-30 19:43:06 -0700

    newlines for good formatting and changed default css folder to styles to match existing folder structure

--- httpdocs/engine/assistants/html.php
+++ httpdocs/engine/assistants/html.php
@@ -3,16 +3,16 @@ function inc($type, $file = null, $folder = null) {
 	switch($type) {
 		default:
 		case "css":
-			$folder = (( $folder == null ) ? "/css/" : $folder );
-			echo "<link rel='stylesheet' type='text/css' href='".$folder.$file.".css' />";
+			$folder = (( $folder == null ) ? "/styles/" : $folder );
+			echo "<link rel='stylesheet' type='text/css' href='".$folder.$file.".css' />\n";
 			break;
 		case "js":
 			$folder = (( $folder == null ) ? "/scripts/" : $folder );
-			echo "<script type='text/javascript' src='".$folder.$file.".js'></script>";
+			echo "<script type='text/javascript' src='".$folder.$file.".js'></script>\n";
 			break;
 		case "jquery":
 		case "jq":
-			echo "<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>";
+			echo "<script type='text/javascript' src='http://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js'></script>\n";
 			break;
 	}
 }
